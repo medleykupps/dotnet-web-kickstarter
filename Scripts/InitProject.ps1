ba878123cbcfd1a7a1e825080e9c76a249dd5b64
Sam Medley <sam.medley@kiandra.com.au>
2017-09-24 11:58:24 +1000

    Updating Init script

--- Scripts/InitProject.ps1
+++ Scripts/InitProject.ps1
@@ -1,34 +1,58 @@
+[CmdletBinding()]
 Param (
     [Parameter(Mandatory=$True,Position=1)]
-    [string] $projectName
+    [string] $projectName,
+
+    [Parameter(Mandatory=$True,Position=2)]
+    [string] $solutionName,
+
+    [Parameter(Mandatory=$False,Position=3)]
+    [string] $rootFolder
 )
-#$projectName = "koowe.Profile"
 
-Write-Progress -Activity "Renaming all project files..."  
+Function RenameSolution
+{
+    # Rename folders and files from KickStarter to $projectName
+    Get-ChildItem -path "$root*" -Recurse -Filter *KickStarter* | Rename-Item -NewName {$_.name -Replace 'KickStarter', $projectName}
+}
 
-$root = "C:\git\Personal\kickstarters\dotnet-web-kickstarter\Src\"
+Function RenameProjectContent
+{
+    # Search for KickStarter inside C# files
+    Get-ChildItem "$root" -Recurse -Include *.cs | `
+        ?{ -not $_.PSIsContainer } | `
+        Select-String -pattern "KickStarter" | ` 
+        Select-Object -unique path | `
+        ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "namespace KickStarter", "namespace $projectName";} | `
+        % {Set-Content $_.Path $_.Content } }
 
-# Rename folders and files from KickStarter to $projectName
-Get-ChildItem -path "$root*" -Recurse -Filter *KickStarter* | Rename-Item -NewName {$_.name -Replace 'KickStarter', $projectName}
+    Get-ChildItem "$root" -recurse -Include *.cs, *.cshtml `
+        | ?{ -not $_.PSIsContainer } `
+        | Select-String -pattern "KickStarter" `
+        | Select-Object -unique path `
+        | ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "using KickStarter", "using $projectName";} `
+        | % {Set-Content $_.Path $_.Content } }
 
-# Search for KickStarter inside C# files
-Get-ChildItem "$root" -Recurse -Include *.cs | `
-    ?{ -not $_.PSIsContainer } | `
-    Select-String -pattern "KickStarter" | ` 
-    Select-Object -unique path | `
-    ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "namespace KickStarter", "namespace $projectName";} | `
-    % {Set-Content $_.Path $_.Content } }
+    Get-ChildItem "$root" -recurse -Include *.cs, *.cshtml, *.config, *.sln, *.csproj `
+        | ?{ -not $_.PSIsContainer } `
+        | Select-String -pattern "KickStarter" `
+        | Select-Object -unique path `
+        | ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "KickStarter", "$projectName";} `
+        | % {Set-Content $_.Path $_.Content } }
 
-Get-ChildItem "$root" -recurse -Include *.cs, *.cshtml `
-    | ?{ -not $_.PSIsContainer } `
-    | Select-String -pattern "KickStarter" `
-    | Select-Object -unique path `
-    | ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "using KickStarter", "using $projectName";} `
-    | % {Set-Content $_.Path $_.Content } }
+}
 
-Get-ChildItem "$root" -recurse -Include *.cs, *.cshtml `
-    | ?{ -not $_.PSIsContainer } `
-    | Select-String -pattern "KickStarter" `
-    | Select-Object -unique path `
-    | ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "KickStarter", "$projectName";} `
-    | % {Set-Content $_.Path $_.Content } }
+
+Write-Progress -Activity "Renaming all project files..."
+
+Write-Debug $("Debug message..." + $PSScriptRoot)
+
+$root = ".\Src"
+if ($rootFolder) {
+    $root = $rootFolder
+}
+
+Write-Debug $("Root: " + $root)
+
+RenameSolution
+
