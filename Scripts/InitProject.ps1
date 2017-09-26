67b84e06d1ea33f1d3b50618be76b8ce8316ec25
Sam Medley <sam.medley@kiandra.com.au>
2017-09-26 22:08:28 +1000

    Updated Init script

--- Scripts/InitProject.ps1
+++ Scripts/InitProject.ps1
@@ -3,17 +3,15 @@ Param (
     [Parameter(Mandatory=$True,Position=1)]
     [string] $projectName,
 
-    [Parameter(Mandatory=$True,Position=2)]
-    [string] $solutionName,
-
-    [Parameter(Mandatory=$False,Position=3)]
+    [Parameter(Mandatory=$False)]
     [string] $rootFolder
 )
 
 Function RenameSolution
 {
     # Rename folders and files from KickStarter to $projectName
-    Get-ChildItem -path "$root*" -Recurse -Filter *KickStarter* | Rename-Item -NewName {$_.name -Replace 'KickStarter', $projectName}
+    Get-ChildItem -path "$root*" -Recurse -Filter *KickStarter* `
+        | Rename-Item -NewName {$_.name -Replace 'KickStarter', $projectName}
 }
 
 Function RenameProjectContent
@@ -42,12 +40,6 @@ Function RenameProjectContent
 
 }
 
-Get-ChildItem "$root" -recurse -Include *.cs, *.cshtml, *.config, *.sln, *.csproj `
-    | ?{ -not $_.PSIsContainer } `
-    | Select-String -pattern "KickStarter" `
-    | Select-Object -unique path `
-    | ForEach { @{Path = $_.Path;Content = (Get-Content($_.Path)) -replace "KickStarter", "$projectName";} `
-    | % {Set-Content $_.Path $_.Content } }
 
 Write-Progress -Activity "Renaming all project files..."
 
@@ -61,4 +53,5 @@ if ($rootFolder) {
 Write-Debug $("Root: " + $root)
 
 RenameSolution
+RenameProjectContent
 
